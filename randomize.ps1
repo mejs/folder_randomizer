612c80161d6d775ce86cac68e8ad169a1e746811
Vladimir Vince <vladimirvince@gmail.com>
2021-08-03 18:07:27 -0400

    Update randomize.ps1

--- randomize.ps1
+++ randomize.ps1
@@ -1,14 +1,13 @@
+#This script reads the directories in the location it's placed in and then opens that directory in Windows Explorer.
+
 #This will store the random names
 $randomNames = [System.Collections.ArrayList]@()
 #This reads the folders names
 $nameList = ls -n -Attributes Directory
-#A loop
-for($x=0;$x -lt 1;$x++){
-    #Generates a random number within the size of the $nameList Array
-    $randomNum = Get-Random -Minimum 1 -Maximum $nameList.Count
-    #Access the random entry within the array. The '-1' is because an array starts at 0.
-    $randomNames.Add($nameList[$randomNum-1])
-}
+#Generates a random number within the size of the $nameList Array
+$randomNum = Get-Random -Minimum 1 -Maximum $nameList.Count
+#Access the random entry within the array. The '-1' is because an array starts at 0.
+$randomNames.Add($nameList[$randomNum-1])
 #Writes the names to the console
 Write-Host $randomNames
 ii $randomNames
